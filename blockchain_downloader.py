1c1a3477b16a2307950c747d6a8fa03fde4792a9
TrustyJAID <TrustyJAID@gmail.com>
2016-12-12 00:13:46 -0700

    Update blockchain_downloader.py

--- blockchain_downloader.py
+++ blockchain_downloader.py
@@ -102,6 +102,7 @@ class dlfn():
         tx = SERVER.decoderawtransaction(rawTx)         # Decodes the raw transaction from RPC
         hexdata = ''                                    # string for collecting hex data
         data = b''                                      # binary data
+        origdata = ''
         regexsearch = ''
         for txout in tx['vout']:                  # Searches json for all vout, failed a few times
             if regexsearch != '':
@@ -118,11 +119,13 @@ class dlfn():
                     data += op.encode('utf8')
 
         print(transaction + check_magic(hexdata), end='\r')  # would have liked multi line prints
+        origdata = data
         try:                                            # a lot of transactions failed here trying to
             length = struct.unpack('<L', data[0:4])[0]  # unpack the binary data so I added parameters
             data = data[8:8+length]                     # to try and extract all data
             self.save_file(hexdata, FILENAME+"hex.txt")       # saves all hex data
             self.save_file(data, FILENAME+"data.txt")         # saves all binary data
+            self.save_file(origdata, FILENAME+"data.txt")         # saves all binary data
             if platform.system() == "Windows":
                 self.save_file(transaction+check_magic(hexdata)+"\r\n", "headerfiles.txt")  # creates a file of transactions and headers
             else:
@@ -131,6 +134,7 @@ class dlfn():
         except:
             self.save_file(hexdata, FILENAME+"fhex.txt")      # This is here to save files when the unpack fails
             self.save_file(data, FILENAME+"fdata.txt")        # if we can figure out how to solve the unpacking
+            self.save_file(origdata, FILENAME+"data.txt")         # saves all binary data
             if platform.system() == "Windows":
                 self.save_file(transaction+check_magic(hexdata)+"\r\n", "headerfiles.txt")
             else:
@@ -211,49 +215,58 @@ class dlfn():
             output.write(dataout)
         output.close()
 
+    def checksum(self, data):
+        """
+        Checksum for multi file upload data
+        """
 
-DEFAULT_MAGIC = {"DOC Header":["d0cf11e0a1b11ae1"],
-                 "DOC Footer":["576f72642e446f63756d656e742e"],
-                 "XLS Header":["d0cf11e0a1b11ae1"],
-                 "XLS Footer":["feffffff000000000000000057006f0072006b0062006f006f006b00"],
-                 "PPT Header":["d0cf11e0a1b11ae1"],
-                 "PPT Footer":["a0461df0"],
-                 "ZIP Header":["504b030414"],
-                 "ZIP Footer":["504b050600"],
-                 "ZIPLock Footer":["504b030414000100630000000000"],
-                 "JPG Header":["ffd8ffe000104a464946000101"],
-                 "GIF Header":["474946383961"],
-                 "GIF Footer":["2100003b00"],
-                 "PDF Header":["25504446"],
-                 "PDF Header":["2623323035"],
-                 "PDF Footer":["2525454f46"],
-                 "Torrent Header":["616e6e6f756e6365"],
-                 "TAR/GZ Header":["1f8b"],
-                 "FLI Header":["0011af"],
-                 "EPUB Header":["504b03040a000200"],
-                 "PNG Header":["89504e470d0a1a0a"],
-                 "8192 Header":["6d51514e42"],
-                 "4096 Header":["6d51494e4246672f"],
-                 "2048 Header":["952e3e2e584b7a"],
-                 "Secret Header":["526172211a0700"],
-                 "RAR Header":["6d51454e424667"],
-                 "UTF8 header":["efedface"],
-                 "OGG Header":["4f676753"],
-                 "WAV Header":["42494646", "57415645"],
-                 "AVI Header":["42494646", "41564920"],
-                 "MIDI Header":["4d546864"],
-                 "7z Header":["377abcaf271c"],
-                 "7z Footer":["0000001706"],
-                 "Wikileaks":["57696b696c65616b73"],
-                 "Julian Assange":["4a756c69616e20417373616e6765"],
-                 "Mendax":["4d656e646178"]}
+        checksum = struct.unpack('<L', data[4:8])[0]
+        if checksum != crc32(data):
+            print('Checksum mismatch; expected %d but calculated %d' % (checksum, crc32(data)))
+        return checksum
+
+
+DEFAULT_MAGIC = {"DOC Header": ["d0cf11e0a1b11ae1"],
+                 "DOC Footer": ["576f72642e446f63756d656e742e"],
+                 "XLS Header": ["d0cf11e0a1b11ae1"],
+                 "XLS Footer": ["feffffff000000000000000057006f0072006b0062006f006f006b00"],
+                 "PPT Header": ["d0cf11e0a1b11ae1"],
+                 "PPT Footer": ["a0461df0"],
+                 "ZIP Header": ["504b030414"],
+                 "ZIP Footer": ["504b050600"],
+                 "ZIPLock Footer": ["504b030414000100630000000000"],
+                 "JPG Header": ["ffd8ffe000104a464946000101"],
+                 "GIF Header": ["474946383961"],
+                 "GIF Footer": ["2100003b00"],
+                 "PDF Header": ["25504446"],
+                 "PDF Header": ["2623323035"],
+                 "PDF Footer": ["2525454f46"],
+                 "Torrent Header": ["616e6e6f756e6365"],
+                 "TAR/GZ Header": ["1f8b"],
+                 "FLI Header": ["0011af"],
+                 "EPUB Header": ["504b03040a000200"],
+                 "PNG Header": ["89504e470d0a1a0a"],
+                 "8192 Header": ["6d51514e42"],
+                 "4096 Header": ["6d51494e4246672f"],
+                 "2048 Header": ["952e3e2e584b7a"],
+                 "Secret Header": ["526172211a0700"],
+                 "RAR Header": ["6d51454e424667"],
+                 "UTF8 header": ["efedface"],
+                 "OGG Header": ["4f676753"],
+                 "WAV Header": ["42494646", "57415645"],
+                 "AVI Header": ["42494646", "41564920"],
+                 "MIDI Header": ["4d546864"],
+                 "7z Header": ["377abcaf271c"],
+                 "7z Footer": ["0000001706"],
+                 "Wikileaks": ["57696b696c65616b73"],
+                 "Julian Assange": ["4a756c69616e20417373616e6765"],
+                 "Mendax": ["4d656e646178"]}
 
 
 def check_magic(hexcode, magic=DEFAULT_MAGIC):
     '''Returns a string listing magic bytes found in the given hexcode and compared against the magic dictionary of keys to lists of values.
 
     This is the hex header search function.  It searches the line of hex for any of these known header hex values.
-    TODO: add private PGP 2048, 4096, 8192 headers
     '''
     return ' '.join('{} Found'.format(key)
                    for key, values in magic.iteritems()
