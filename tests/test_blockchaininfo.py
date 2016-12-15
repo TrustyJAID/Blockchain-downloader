982f2b9431f186ea0397a9b6371bd4e4109ead62
That Merlin Guy <thatmerlinguy>
2016-12-15 07:46:55 +0000

    Add tests/

---
+++ tests/test_blockchaininfo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''blockchaininfo test functions'''
+
+import pytest
+
+def test_true():
+    assert True == True
---
+++ tests/test_cli.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''cli test functions'''
+
+import pytest
+
+def test_true():
+    assert True == True
