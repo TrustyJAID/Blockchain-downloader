d9abcee6c4544f8a7504955b0acfa968e5447c18
That Merlin Guy <thatmerlinguy>
2016-12-16 07:45:11 +0000

    Update __VERSION__ to 0.2.0 and include blockchaininfo in wlffbd/__init__.py

--- wlffbd/__init__.py
+++ wlffbd/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 '''wlffbd: WikiLeaks Freedom Force Blockchain Downloader'''
 
-__VERSION__ = '0.1.0'
+__VERSION__ = '0.2.0'
 
 from . import cli
+from . import blockchaininfo
 from . import dlfn
 from . import magic
 
 __all__ = ['cli',
+           'blockchaininfo',
            'dlfn',
            'magic']
