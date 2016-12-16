b2143436ec3ba255733bbf2de69b2857ea473a36
That Merlin Guy <thatmerlinguy>
2016-12-16 10:44:20 +0000

    Add tests for wlffbd.magic

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 '''magic test functions'''
 
+from wlffbd import magic
+
 import pytest
 
-def test_true():
-    assert True == True
+def test_check_magic():
+    assert 'Julian Assange Found' == magic.check_magic("01234567894a756c69616e20417373616e67650123456789")
+    assert ' '.join('{} Found'.format(k) for k in magic.DEFAULT_MAGIC.keys()) == magic.check_magic(''.join(''.join(v) for v in magic.DEFAULT_MAGIC.values()))
